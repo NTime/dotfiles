eda1588b211401a02820fd8e4211e91853acb3c5
NTime <filipe.porangaba@gmail.com>
2023-04-19 13:10:39 -0300

    first commit

---
+++ scripts/.scripts/fullBackup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo rsync -aAXHSv --delete --exclude={"/hard_drive/","/home/luis/Downloads/torrents/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /hard_drive/backup/
